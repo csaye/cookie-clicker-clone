75b9d6f39b5f2c6555756d256ef3e672344fff2e
Cooper Saye <coopersaye@gmail.com>
2020-10-11 18:21:20 -0700

    Implement cookie counter.

--- script.js
+++ script.js
@@ -3,16 +3,17 @@
 
 function get(id) { return document.getElementById(id); }
 
+let cookies = 0;
+
 const cookie = get('cookie');
+const cookieCounter = get('cookie-counter');
+
+function updateCookieCounter() {
+    cookieCounter.innerHTML = `${cookies} cookies`;
+}
 
 cookie.onclick = function() {
-    
+    cookies++;
+    updateCookieCounter();
 }
 
-cookie.onmouseover = function() {
-    cookie.style.backgroundColor = "black";
-}
-
-cookie.onmouseout = function() {
-    cookie.style.backgroundColor = "transparent";
-}
