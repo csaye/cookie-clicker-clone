cac04cc62ef3e96f8a2ae3396630bdfcbc99a1bf
Cooper Saye <coopersaye@gmail.com>
2020-10-11 18:11:45 -0700

    Implement javascript hover detection.

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ const cookie = get('cookie');
 cookie.onclick = function() {
     
 }
+
+cookie.onmouseover = function() {
+    cookie.style.backgroundColor = "black";
+}
+
+cookie.onmouseout = function() {
+    cookie.style.backgroundColor = "transparent";
+}
